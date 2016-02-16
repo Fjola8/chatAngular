0b14267852ca7e5cf7beb5e5913cdd840d255f2f
Marin Jonsdottir <marinjonsdottir@gmail.com>
2016-02-16 16:24:54 +0000

    roomlist og css

--- client/src/roomlist/RoomlistController.js
+++ client/src/roomlist/RoomlistController.js
@@ -1,13 +1,25 @@
+//angular.module("chatApp").controller("RoomlistController", 
+//	function RoomlistController($scope, socket){
 
-angular.module("chatApp").controller("RoomlistController", 
-	function RoomlistController(){
-		//þarf að eiga lista af gögnum - þurfum að sækja hann frá bakenda
-		$scope.roomlist = [ {
-			name: "spjall",
-			id: 1
-		},
-		{
-			name: "fjola",
-			id: 2
-		}];
-	})
+angular.module('chatApp').controller('RoomlistController', ["$scope", "socket", "$location", 
+	function ($scope, socket, $location) {		
+		
+		
+  	socket.on("roomlist", function(roomlist) { //senda inn fall í socket-ið
+			//segja við socket-ið að þegar þessi roomlist event kemur inn, þá á það að kalla á þetta fall
+			//erum að senda inn fall sem færibreytu
+
+    		
+    		$scope.$apply(function() { //til að uppfæra UI strax
+
+					$scope.roomlist = roomlist; //tek breytuna sem fallið tekur inn og set inn í scopeið
+
+					//console.log(roomlist);
+					// til að keyra síðuna og fara síðan í F12 til að skoða objectin sem eru prentuð út
+					// þá getum við séð hvernig objectið er sett upp og gert html út frá því - er object sem heitir numParticipants?
+				}
+  	});
+
+	socket.emit("roomlist");
+
+});
