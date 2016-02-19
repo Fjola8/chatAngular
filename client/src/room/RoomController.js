49a586442a4f71dc6c6d2ceae27c89597f80df64
annagudrun <annag14@ru.is>
2016-02-19 15:45:50 +0000

    users in chat and leave room

--- client/src/room/RoomController.js
+++ client/src/room/RoomController.js
@@ -7,22 +7,19 @@ angular.module('chatApp').controller('RoomController', ["$scope", "socket", "$lo
 		$scope.messageHistory = [];
 		$scope.currUsers = [];
 
-
 		socket.emit('joinroom', {'room': $scope.roomName});
+
 
 		socket.on('updateusers', function(room, users, ops) {
             socket.emit('users');
             socket.emit('rooms');
             socket.emit('getUserChannels');
+						if(room == $scope.roomName){
+						$scope.currUsers = users;
+					}
     	});
 
-        /*// Fetch the chat history for the current room.
-            socket.on('updatechat', function(roomName, history) {
-                if ($scope.curUserChannels[roomName] !== undefined) {
-                    $scope.curUserChannels[roomName].messageHistory = history;
-                }
-            });*/
-
+			//messageHistory er til í server
 		socket.on('updatechat', function(room, chatHistory){
 			if(room == $scope.roomName){
 				$scope.messageHistory = chatHistory;
@@ -34,28 +31,10 @@ angular.module('chatApp').controller('RoomController', ["$scope", "socket", "$lo
  		    $scope.message = null; //hreinsa textabox
 		};
 
-        $scope.leaveRoom = function(channel) {
-       //    $socket.emit('partroom', channel);
+        $scope.leaveRoom = function() {
+           socket.emit('partroom', $scope.roomName);
            $location.path("/roomlist/" + $scope.currUser);
         };
 
-      	socket.emit("users");
-  		socket.on("userlist", function(users) {
-    		$scope.activeUsers = users;
-  		});
+
  }]);
-
-
-
-
-/*angular.module("chatApp").controller("RoomController",
-	function RoomController($scope, $routeParams){
-		//RoomController þarf að geta nálgast það hvaða id er í urlinu -> routeParams
-		var id = $routeParams.id;
-		//svo í app.js --> .when("/rooms/:id", {..}) - :id og routeParams.id þarf að passa
-	});
-
-
-
-	{roomName: "the room identifier", msg: "The message itself, only the first 200 chars are considered valid" }
-*/
